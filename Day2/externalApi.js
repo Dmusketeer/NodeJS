6b90101367e7545f07314d60ada92eec9f23ce49
Dmusketeer <ardmountronics@gmail.com>
2021-05-20 13:36:14 +0530

    modified

--- Day2/externalApi.js
+++ Day2/externalApi.js
@@ -1,5 +1,15 @@
 // const superhero=require('superhero');
 // console.log(superhero.random());
-
-
-// Create own server
+// Create own server:
+const http = require('http');
+const hostname = "localhost";
+const port = 3000;
+const server = http.createServer((req, res)=>{
+    console.log(req.headers);
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/http');
+    res.end('<html><body><h1> Server connection success:) </h1></body></html>');
+});
+server.listen(port,hostname,()=>{
+    console.log(`server running at http://${hostname}:${port}`);
+});
