06635d5ff7f6daadde555291261dce052cda857f
Dmusketeer <ardmountronics@gmail.com>
2021-05-22 23:37:12 +0530

    file system in node

---
+++ Day4/script1.js
@@ -0,0 +1,8 @@
+var exp=require('./script');
+
+console.log(`The area of circle is : ${exp.Area(10)}`);
+console.log(`The area of rectangle is : ${exp.area(10,20)}`);
+console.log(`The perimeter of rectangle is : ${exp.perimeter(10,20)}`);
+console.log(`The  current date and time is : ${exp.currentDateTime}`);
+console.log(`The  directory name is : ${exp.directoryName}`);
+console.log(`The  file name is : ${exp.fileName}`);
