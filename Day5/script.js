2a08abee5d670df411f38ea44083b3b1d99feae5
Dmusketeer <ardmountronics@gmail.com>
2021-07-02 00:26:34 +0530

    js file

--- Day5/script.js
+++ Day5/script.js
@@ -1,7 +1,18 @@
-let http= require('http');
-var server=http.createServer((req,res)=>{
-    res.writeHead('this is response for a request.');
-    res.end();
-});
-server.listen(3000);
+// let http= require('http');
+// var server=http.createServer((req,res)=>{
+//     res.writeHead('this is response for a request.');
+//     res.end();
+// });
+// server.listen(3000);
 
+
+async function fetchData(){
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data= await res.json();
+    for (let index = 0; index <10; index++) {
+        var node=document.createElement("li");
+        node.innerHTML=data[index].title;
+        document.body.appendChild(node);        
+    }
+}
+fetchData();
