632a19bc6f41f4aeb38ad9ae18f8dfdce3a8e973
Dmusketeer <ardmountronics@gmail.com>
2021-07-25 17:22:43 +0530

    forms

--- Express/form/app.js
+++ Express/form/app.js
@@ -1,26 +1,51 @@
-var express = require('express');
-var path = require('path');
-var mongodb = require('mongodb');
-var bodyParser = require('body-parser');
-var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myDB');
+var db = mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function (callback) {
+    console.log("connection succeeded");
+})
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.post('/post-feedback', function (req, res) {
-    dbConn.then(function (db) {
-        delete req.body._id; // for safety reasons
-        db.collection('feedbacks').insertOne(req.body);
+var app = express()
+
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/sign_up', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var pass = req.body.password;
+    var phone = req.body.phone;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "password": pass,
+        "phone": phone
+    }
+    db.collection('details').insertOne(data, function (err, collection) {
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+
     });
-    res.send('Data received:\n' + JSON.stringify(req.body));
-});
-app.get('/view-feedbacks', function (req, res) {
-    dbConn.then(function (db) {
-        db.collection('feedbacks').find({}).toArray().then(function (feedbacks) {
-            res.status(200).json(feedbacks);
-        });
-    });
-});
 
-app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0');
+    return res.redirect('signup_success.html');
+})
+
+
+app.get('/', function (req, res) {
+    res.set({
+        'Access-control-Allow-Origin': '*'
+    });
+    return res.redirect('./index.html');
+}).listen(3000)
+
+
+console.log("server listening at port 3000");
