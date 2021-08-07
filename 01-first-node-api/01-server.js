a73c2a2db493e53a9ebe72de4322d973c841223f
Dmusketeer <ardmountronics@gmail.com>
2021-08-07 22:16:11 +0530

    modified

--- 01-first-node-api/01-server.js
+++ 01-first-node-api/01-server.js
@@ -9,7 +9,12 @@ const http= require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res)=>{
-    res.end("<h1>Hey Dheeraj! </h1>");
+    // create simple messages
+    // res.end("<h1>Hey Dheeraj! </h1>");
+    
+// serving JSON
+res.setHeader('Content-Type','appliction/json');
+res.end(JSON.stringify({name:"Dheeraj",age:27}));
 })
 
 server.listen(port,()=>{
