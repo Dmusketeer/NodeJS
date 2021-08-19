76c7c8e65c538d05924d66d121e0808b74bcc75b
Dmusketeer <ardmountronics@gmail.com>
2021-08-19 18:20:50 +0530

    routing in the nodejs

---
+++ routingInPureNodejs/server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    let route = "./views/";
+
+    switch (req.url) {
+        case '/':
+            route += 'index.html';
+            break;
+        case '/contact':
+            route += 'contact.html';
+            break;
+
+        default:
+            route += '404.html';
+            break;
+    }
+
+    fs.readFile(route, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.end(data);
+
+        }
+    })
+});
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
