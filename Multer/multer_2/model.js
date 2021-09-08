a2eda5451497d24a0eb794656dfd00c200e93527
Dmusketeer <ardmountronics@gmail.com>
2021-09-08 09:36:01 +0530

    model

---
+++ Multer/multer_2/model.js
@@ -0,0 +1,18 @@
+
+// Step 3 - this is the code for ./models.js
+
+var mongoose = require('mongoose');
+
+var imageSchema = new mongoose.Schema({
+    name: String,
+    desc: String,
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+//Image is a model which has a schema imageSchema
+
+module.exports = new mongoose.model('Image', imageSchema);
