4fc11ed75ba7f4b19b5c4c5208c550169d27d982
Dmusketeer <ardmountronics@gmail.com>
2021-09-15 22:12:27 +0530

    send email using node mailer

---
+++ email-password-reset/utils/sendEmail.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+// const router = express.Router();
+const sendEmail = async (email, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            service: process.env.SERVICE,
+            port: 587,
+            secure: true,
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS,
+            },
+        });
+
+        await transporter.sendMail({
+            from: process.env.USER,
+            to: email,
+            subject: subject,
+            text: text,
+        });
+
+        console.log("email sent sucessfully");
+    }
+    catch {
+        console.log(error, "email not sent");
+    }
+}
+
+
+module.exports = sendEmail;
