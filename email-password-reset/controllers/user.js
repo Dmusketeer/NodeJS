10c6609c0aa9f0e10574a6a4d75fe312e14ff921
Dmusketeer <ardmountronics@gmail.com>
2021-09-15 22:14:38 +0530

    user controllers

---
+++ email-password-reset/controllers/user.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const { User, validate } = require('../models/user')
+
+module.exports.Save = async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const user = await new User(req.body).save();
+
+        res.send(user);
+    } catch (error) {
+        res.send("An error occured");
+        console.log(error);
+    }
+}
