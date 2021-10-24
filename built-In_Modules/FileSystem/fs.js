c6f1e3bf0ef7898f421d63c40596263239a4c214
Dmusketeer <ardmountronics@gmail.com>
2021-10-24 16:25:53 +0530

    writing file

--- built-In_Modules/FileSystem/fs.js
+++ built-In_Modules/FileSystem/fs.js
@@ -1,14 +1,25 @@
 const fs = require('fs')
 
-
+// =================================== reading files
 // Asysnchronous read
-fs.readFile('text.txt', (err, data) => {
+// fs.readFile('text.txt', (err, data) => {
+//     if (err) console.log(err);
+//     // console.log(data); // will return buffer
+//     console.log(data.toString());
+// })
+
+// // Synchronous read
+// const data = fs.readFileSync('text.txt')
+// console.log(data.toString());
+
+
+// Writing to the files
+var data1 = 'Hello how are you ! what\'s Going On.'
+fs.writeFile('text1.txt', data1, (err) => {
     if (err) console.log(err);
-    // console.log(data); // will return buffer
-    console.log(data.toString());
+    console.log("data written successfully!");
+    fs.readFile('text1.txt', (err, data) => {
+        if (err) console.log(err);
+        console.log(data.toString());
+    })
 })
-
-
-// Synchronous read
-const data = fs.readFileSync('text.txt')
-console.log(data.toString());
