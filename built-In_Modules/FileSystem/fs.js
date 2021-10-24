a3fa32ad52fb8891c7b7b4ab755d3140dbcfc999
Dmusketeer <ardmountronics@gmail.com>
2021-10-24 16:57:13 +0530

    open and unlink

--- built-In_Modules/FileSystem/fs.js
+++ built-In_Modules/FileSystem/fs.js
@@ -14,12 +14,38 @@ const fs = require('fs')
 
 
 // Writing to the files
-var data1 = 'Hello how are you ! what\'s Going On.'
-fs.writeFile('text1.txt', data1, (err) => {
-    if (err) console.log(err);
-    console.log("data written successfully!");
-    fs.readFile('text1.txt', (err, data) => {
-        if (err) console.log(err);
-        console.log(data.toString());
+// var data1 = 'Hello how are you ! what\'s Going On.'
+// fs.writeFile('text1.txt', data1, (err) => {
+//     if (err) console.log(err);
+//     console.log("data written successfully!");
+//     fs.readFile('text1.txt', (err, data) => {
+//         if (err) console.log(err);
+//         console.log(data.toString());
+//     })
+// })
+
+
+
+// open a file
+fs.open('text1.txt', 'r+', (err, fd) => {
+    if (err) console.log(err)
+    console.log(fd);
+    console.log("File open successfully");
+    // Close the open file
+    fs.close(fd, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("File closed successfully.");
     })
 })
+
+
+
+// delete the file
+
+fs.unlink('text1.txt', function (err) {
+    console.log(err);
+    console.log("file Successfully deleted");
+})
+
